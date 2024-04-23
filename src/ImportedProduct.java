a3400185c597a3ee2225d06ebc9101c064cf72f9
jabes <138511008+jabesz@users.noreply.github.com>
2024-04-23 13:07:05 -0300

    Update ImportedProduct.java

--- src/ImportedProduct.java
+++ src/ImportedProduct.java
@@ -5,25 +5,21 @@ public class ImportedProduct extends Product{
         this.customsFee = customsFee;
     }
 
-    public ImportedProduct(String name, Double price, Double customsFee) {
+    public ImportedProduct(String name, double price, double customsFee) {
         super(name, price);
         this.customsFee = customsFee;
     }
 
-    public Double getCustomsFee() {
+    public double getCustomsFee() {
         return customsFee;
     }
 
-    public void setCustomsFee(Double customsFee) {
-        this.customsFee = customsFee;
+    public double customFeeAdd() {
+        return customsFee + getPrice();
     }
 
     @Override
     public String priceTag() {
-        return getName() + " $ " + String.format("%2.f",totalPrice()) + "Custom fee: $" + String.format("%2.f", getCustomsFee()) + ")";
-    }
-
-    public Double totalPrice() {
-        return totalPrice();
+        return super.priceTag() + " Price: $" + customFeeAdd() +" (Customs fee: $ " + String.format("%.2f", getCustomsFee()) + ")";
     }
 }
